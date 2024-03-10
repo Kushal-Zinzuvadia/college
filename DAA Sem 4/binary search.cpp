664356354fdd28c789d961ba49400a9bc0fde13f
Kushal Zinzuvadia <128955646+Kushal-Zinzuvadia@users.noreply.github.com>
2024-03-10 15:24:56 +0530

    Update binary search.cpp

--- DAA Sem 4/binary search.cpp
+++ DAA Sem 4/binary search.cpp
@@ -5,15 +5,12 @@ int binarySearch(int array[], int x, int low, int high) {
   if (high >= low) {
     int mid = low + (high - low) / 2;
 
-    // If found at mid, then return it
     if (array[mid] == x)
       return mid;
 
-    // Search the left half
     if (array[mid] > x)
       return binarySearch(array, x, low, mid - 1);
 
-    // Search the right half
     return binarySearch(array, x, mid + 1, high);
   }
 
